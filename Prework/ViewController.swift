a192be2d1e0ad3bbf8d03e4420697f7bcfa112c4
Ngoc N. Hoang <nnh245@nyu.edu>
2021-08-11 18:04:06 +0400

    First commit

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UITextField!
+    @IBOutlet weak var tipControl: UISlider!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func selectTip(_ sender: UISlider) {
+        let tipPerc = sender.value
+        tipPercentageLabel.text = String(format: "%.0f%%", tipPerc)
+    }
+//    @IBAction func calculateTip(_ sender: Any) {
+//        let bill = Double(billAmountTextField.text!) ?? 0
+//        let tipPerc = Double(tipControl.value / 100)
+//        let tip = bill * tipPerc
+//        let total = bill + tip
+//
+//        tipAmountLabel.text = String(format: "$%.2f", tip)
+//        totalLabel.text = String(format: "$.2f", total)
+//    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPerc = Double(tipControl.value / 100)
+        let tip = bill * tipPerc
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func tipCalculate(_ sender: UITextField) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPerc = Double(tipControl.value / 100)
+        let tip = bill * tipPerc
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
